74906f5f22cf411a35c5b6183a7f118208dd8bee
Christian047 <chrispadiga@gmail.com>
2024-07-03 08:13:54 +0100

    New commit

--- api_weather_app/views.py
+++ api_weather_app/views.py
@@ -1,34 +1,44 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 import requests
-import json
-
 
 IPINFO_TOKEN = 'de618f7d55f0ee'
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
+    # PythonAnywhere specific header
+    ip = request.META.get('HTTP_X_REAL_IP')
+    if not ip:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
     return ip
+
 @require_GET
 def hello_api(request):
     visitor_name = request.GET.get('visitor_name', 'Guest')
     client_ip = get_client_ip(request)
 
     try:
-        # Get weather data using the client's IP address directly
-        api_key = '3256268559ec8bacb647c6d9cbf7e5ef'
-        weather_url = f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&units=metric'
+        # Get location data from ipinfo.io
+        ip_url = f'https://ipinfo.io/{client_ip}/json?token={IPINFO_TOKEN}'
+        ip_response = requests.get(ip_url)
+        
+        if ip_response.status_code == 200:
+            ip_data = ip_response.json()
+            city = ip_data.get('city', 'Unknown')
+        else:
+            return JsonResponse({'error': 'Failed to fetch IP and geolocation data'}, status=ip_response.status_code)
 
-        # Directly use client_ip for geolocation in OpenWeatherMap
-        weather_response = requests.get(f'{weather_url}&ip={client_ip}')
+        # Get weather data
+        api_key = '3256268559ec8bacb647c6d9cbf7e5ef'
+        weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+
+        weather_response = requests.get(weather_url)
         weather_data = weather_response.json()
 
         if weather_response.status_code == 200:
-            city = weather_data.get('name', 'Unknown')
             temperature = weather_data['main']['temp']
 
             greeting = f"Hello, {visitor_name}! The temperature is {temperature:.0f} degrees Celsius in {city}"
